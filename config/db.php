3447fe1734eed04cb9c7841871391f77dbac009a
Marta Tirador <martatiradorgutierrez@gmail.com>
2024-06-23 22:40:28 +0200

    remove file + image

--- config/db.php
+++ config/db.php
@@ -3,11 +3,12 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=db;dbname=ikigai2',
-    'username' => 'user', // Asegúrate de usar el mismo usuario definido en docker-compose.yml
-    'password' => 'password', // Asegúrate de usar la misma contraseña definida en docker-compose.yml
+    'username' => 'user', // Username defined in docker-compose.yml
+    'password' => 'password', // Password defined in docker-compose.yml
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
+    // Uncomment the following lines for production:
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
